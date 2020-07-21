d60bf17ecbff2595d722633468143f2a2f4ccb94
Rushi Panchariya <rushirock2000@outlook.com>
2020-07-21 23:08:48 +0530

    Final changes

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss'],
+	selector: 'app-footer',
+	templateUrl: './footer.component.html',
+	styleUrls: [ './footer.component.scss' ]
 })
 export class FooterComponent implements OnInit {
-  emailadd: string = 'rushipanchariya19@gmail.com';
-  currentYear = new Date();
-  year: number;
+	emailadd: string = 'rushipanchariya19@gmail.com';
+	currentYear = new Date();
+	year: number;
 
-  constructor() {}
-  ngOnInit(): void {
-    this.year = this.currentYear.getFullYear();
-  }
+	constructor() {}
+	ngOnInit(): void {
+		this.year = this.currentYear.getFullYear();
+	}
+	knowMore(section) {
+		document.querySelector('#' + section).scrollIntoView({ block: 'start', behavior: 'smooth' });
+	}
 }
